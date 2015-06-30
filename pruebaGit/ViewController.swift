6bd9768307f38194994b6f3c7dd3f4e5dc14f764
Rafael Lozano <ralozano@itesm.mx>
2015-06-29 21:26:50 -0500

    Mi segunda validación

--- pruebaGit/ViewController.swift
+++ pruebaGit/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // Cambio 1
+        // Cambio 2
     }
 
     override func didReceiveMemoryWarning() {
